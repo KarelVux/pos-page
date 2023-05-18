a5c49d8293ee754c2964ddfe385e45a0fa11035c
KarelVux <70542555+KarelVux@users.noreply.github.com>
2023-05-18 23:30:48 +0300

    Some invoice ideas

--- src/views/store/InvoiceDetailsView.vue
+++ src/views/store/InvoiceDetailsView.vue
@@ -1,58 +1,86 @@
 <template>
-    <div class="container mt-6 mb-7">
-        <div class="row justify-content-center">
-            <div class="col-lg-12 col-xl-7">
-                <div class="card">
-                    <div class="card-body p-5">
-                        <h2>
-                            Hey ,
-                        </h2>
-                        <p class="fs-sm">
-                            This is the receipt for a payment of <strong>$312.00</strong> (USD) you made to Spacial Themes.
-                        </p>
+  <div class="container mt-6 mb-7">
+    <div class="row justify-content-center">
+      <div class="col-lg-12 col-xl-7">
+        <div class="card">
+          <div class="card-body p-5">
+            <h2>
+              Hey ,
+            </h2>
+            <p class="fs-sm">
+              This is the receipt for a payment of <strong>{{ invoiceData.finalTotalPrice }}</strong>you made!
+            </p>
 
-                        <table class="table border-bottom border-gray-200 mt-3">
-                            <thead>
-                            <tr>
-                                <th scope="col" class="fs-sm text-dark text-uppercase-bold-sm px-0">Description</th>
-                                <th scope="col" class="fs-sm text-dark text-uppercase-bold-sm text-end px-0">Amount</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td class="px-0">Theme customization</td>
-                                <td class="text-end px-0">$60.00</td>
-                            </tr>
-                            <tr>
-                                <td class="px-0">Website design</td>
-                                <td class="text-end px-0">$80.00</td>
-                            </tr>
-                            </tbody>
-                        </table>
+            <table class="table border-bottom border-gray-200 mt-3">
+              <thead>
+              <tr>
+                <th scope="col" class="fs-sm text-dark text-uppercase-bold-sm px-0">Description</th>
+                <th scope="col" class="fs-sm text-dark text-uppercase-bold-sm text-end px-0">Amount</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="invoiceRow in invoiceData.invoiceRowDatas" :key="invoiceRow.id">
+                <td class="px-0">{{ invoiceRow.productName }}</td>
+                <td class="text-end px-0">{{ invoiceRow.finalProductPrice }} {{ invoiceRow.currency }}</td>
+              </tr>
+              </tbody>
+            </table>
 
-                        <div class="mt-5">
-                            <div class="d-flex justify-content-end">
-                                <p class="text-muted me-3">Subtotal:</p>
-                                <span>$390.00</span>
-                            </div>
-                            <div class="d-flex justify-content-end">
-                                <p class="text-muted me-3">Discount:</p>
-                                <span>-$40.00</span>
-                            </div>
-                            <div class="d-flex justify-content-end mt-3">
-                                <h5 class="me-3">Total:</h5>
-                                <h5 class="text-success">$399.99 USD</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#!" class="btn btn-dark btn-lg card-footer-btn justify-content-center text-uppercase-bold-sm hover-lift-light">
-                        Pay Now
-                    </a>
-                </div>
+            <div class="mt-5">
+<!--              <div class="d-flex justify-content-end">
+                <p class="text-muted me-3">Subtotal:</p>
+                <span>{{invoiceData.totalPriceWithoutTax}}</span>
+              </div>
+
+              -->
+              <div class="d-flex justify-content-end mt-3">
+                <h5 class="m<
+
+                !---->e-3">Total:</h5>
+                <h5 class="text-success">{{ invoiceData.finalTotalPrice }}</h5>
+              </div>
             </div>
+          </div>
+          <a href="#!"
+             class="btn btn-dark btn-lg card-footer-btn justify-content-center text-uppercase-bold-sm hover-lift-light">
+            Pay Now
+          </a>
         </div>
-    </div></template>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script lang="ts" setup>
+import {useIdentityStore} from "@/stores/identityStore";
+import ShopsService from "@/services/shop/ShopsService";
+import {useRoute} from "vue-router";
+import {onBeforeMount, ref} from "vue";
+import type {IInvoiceData} from "@/dto/shop/IInvoiceData";
+
+const identitySore = useIdentityStore();
+const shopsService = new ShopsService();
+
+const route = useRoute();
+const invoiceData = ref<IInvoiceData>()
+
+onBeforeMount(async () => {
+  console.log("Open business details")
+  let identity = identitySore.authenticationJwt;
+
+
+  if (identity === undefined) {
+    console.log("jwt is null")
+    return;
+  }
+
+  if (route.params.id) {
+    invoiceData.value = (await shopsService.getInvoice(identity, route.params.id as string))
+    console.log("Invoice details", invoiceData)
+  } else {
+    console.error("Invoice  id is not initialized")
+  }
+
+})
 
 </script>
